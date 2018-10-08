fa9ee23d4fa189f0259da0f7048464c5390ca30a
Alexander <artoriousso@gmail.com>
2018-10-09 00:05:53 +0300

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, only: :show do
+  resources :users, only: [:show, :update] do
     member do
-      get 'password'
+      get 'pin'
+      get 'main_screen'
+      get 'take_cash'
+      get 'put_cash'
+      get 'transaction'
+      post 'pin_check'
+      post 'card_number_check'
     end
+  end
+  scope :users do
+    get 'insert_card', to: 'users#insert_card'
   end
 end

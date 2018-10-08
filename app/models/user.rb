4305d4aef639e72a35025b8dadbf041fbda690e5
Alexander <artoriousso@gmail.com>
2018-10-08 15:36:16 +0300

    Add status to user and default value for attempts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  enum status: [:valid, :blocked]
+
+  validates :card_number, presence: true
+  validates :pin,         presence: true
+  validates :balance,     presence: true
+  validates :attempts,    presence: true
 end
--- db/migrate/20181008112826_add_fields_to_user.rb
+++ db/migrate/20181008112826_add_fields_to_user.rb
@@ -3,6 +3,6 @@ class AddFieldsToUser < ActiveRecord::Migration[5.2]
     add_column :users, :card_number, :integer
     add_column :users, :pin, :integer
     add_column :users, :balance, :integer
-    add_column :users, :attempts, :integer
+    add_column :users, :attempts, :integer, default: 0
   end
 end

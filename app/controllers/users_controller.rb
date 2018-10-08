241456cecac2ec8b6938865b3fa365e7482364d2
Alexander <artoriousso@gmail.com>
2018-10-09 00:07:30 +0300

    Describe basic actions and methods in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,54 @@
 class UsersController < ApplicationController
-  # before_action :show
+  before_action :user, except: :insert_card
+  # before_action :check_pin, only: :main_screen
 
-  def password
-    render :password
+  def insert_card
+    render :insert_card
+  end
+
+  def pin
+    render :pin
+  end
+
+  def pin_check
+
+  end
+
+  def card_number_check
+    
   end
 
   def main_screen
-
+    render :main_screen
   end
 
   def show
     render :show
   end
 
-  def cash_advance
-
+  def take_cash
+    render :take_cash
   end
 
   def put_cash
-
+    render :put_cash
   end
 
   def transaction
+    render :transaction
+  end
+
+  def update
 
   end
 
   private
 
   def user
-
+    @user ||= User.find(params[:id])
   end
 
+  def user_params
 
+  end
 end
-

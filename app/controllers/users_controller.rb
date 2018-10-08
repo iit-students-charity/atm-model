c8270b336b4dd89a236763e874a72e2483475438
Alexander <artoriousso@gmail.com>
2018-10-08 17:00:07 +0300

    Create users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  # before_action :show
+
+  def password
+    render :password
+  end
+
+  def main_screen
+
+  end
+
+  def show
+    render :show
+  end
+
+  def cash_advance
+
+  end
+
+  def put_cash
+
+  end
+
+  def transaction
+
+  end
+
+  private
+
+  def user
+
+  end
+
+
+end
+

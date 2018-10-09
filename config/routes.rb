4971d94c3ec3f7697f16f60be57c4296a8d2ea67
Alexander <artoriousso@gmail.com>
2018-10-09 12:38:27 +0300

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  scope :users do
+    get 'insert_card', to: 'users#insert_card'
+    post 'card_number_check', to: 'users#card_number_check'
+  end
+
   resources :users, only: [:show, :update] do
     member do
       get 'pin'
@@ -7,10 +12,6 @@ Rails.application.routes.draw do
       get 'put_cash'
       get 'transaction'
       post 'pin_check'
-      post 'card_number_check'
     end
-  end
-  scope :users do
-    get 'insert_card', to: 'users#insert_card'
   end
 end

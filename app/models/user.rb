d7a7a95e266fe3555470bb66443504cb06027432
Alexander <artoriousso@gmail.com>
2018-10-09 16:36:11 +0300

    Add methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,31 @@ class User < ApplicationRecord
   validates :pin,         presence: true
   validates :balance,     presence: true
   validates :attempts,    presence: true
+  validates :status,      presence: true
+
+  def correct_pin
+    unlock! if active?
+  end
+
+  def incorrect_pin
+    update_attributes(attempts: attempts + 1) unless block_required?
+    block! if block_required?
+  end
+
+  def block_required?
+    attempts >= 3
+  end
+
+  def reset_attempts
+    update_attributes(attempts: 0)
+  end
+
+  def block!
+    update_attributes(status: :blocked)
+  end
+
+  def unlock!
+    reset_attempts
+    update_attributes(status: :active)
+  end
 end

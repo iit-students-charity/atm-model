9a236b05ed567ff1536d2b2c7c1424d1035612d1
Alexander <artoriousso@gmail.com>
2018-10-15 14:40:32 +0300

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(card_number: 1111222233334444, pin: 1111, balance: 1000, attempts: 0)
+User.create(card_number: 2222333344445555, pin: 2222, balance: 500, attempts: 0)
+User.create(card_number: 3333444455556666, pin: 3333, balance: 100, attempts: 0)
+User.create(card_number: 4444555566667777, pin: 4444, balance: 10, attempts: 0)
+User.create(card_number: 5555666677778888, pin: 5555, balance: 0, attempts: 0)
